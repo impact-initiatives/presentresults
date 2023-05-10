59776e8f6c9522a29099f5ce884411d584e4c68e
yannsay-impact <yann.say@impact-initiatives.org>
2023-05-10 11:49:05 +0200

    changing NA to "NA" in test (new version?)

--- tests/testthat/test-create_analysis_key_table.R
+++ tests/testthat/test-create_analysis_key_table.R
@@ -6,11 +6,11 @@ test_that("returns correct results", {
   expected_output <- results_test %>%
     dplyr::mutate(analysis_type = c("prop_select_one", "mean"),
       analysis_var_1 = c("fcs_cat", "expenditure_food"),
-                  analysis_var_value_1 = c("low", NA),
+                  analysis_var_value_1 = c("low", "NA"),
                   group_var_1 = c("location", "location"),
                   group_var_value_1 = c("locationA", "locationB"),
-                  group_var_2 = c("population", NA),
-                  group_var_value_2 = c("displaced", NA),
+                  group_var_2 = c("population", "NA"),
+                  group_var_value_2 = c("displaced", "NA"),
       nb_analysis_var = c(1,1),
       nb_group_var = c(2,1))
 
