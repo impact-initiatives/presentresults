40a5dd7381b13f7ca55001b6605597914568c72a
Md Mehedi Hasan KHAN <plannermehedi@gamil.com>
2023-06-07 19:57:29 +0300

    adding_create_group_cluster

---
+++ R/create_cluster_and_strata_summary.R
@@ -0,0 +1,62 @@
+
+#' Create number of cluster and number of hh surveyed per group/strata
+#'
+#' @param result A result table which must contain `analysis_key` column.
+#' @param analysis_key String with the name of the analysis key. Default is "analysis_key"
+#' @param dataset dataset
+#' @param cluster_name String with the name of the cluster id in the dataset.
+#' @return A dataframe with number of HH and cluster in each group
+#' @export
+#'
+#' @examples
+#' create_group_clusters(result = presentresults_resultstable,
+#' dataset = presentresults_MSNA_template_data,
+#' cluster_name = "cluster_id")
+#'
+ create_group_clusters <- function(result,
+                                  analysis_key ="analysis_key",
+                                  dataset,
+                                  cluster_name) {
+
+  if(!analysis_key %in% names(result)){stop(glue::glue("Analysis key: ",{{analysis_key}}, " is not found in the result table."))}
+  if(!cluster_name %in% names(dataset)){stop(glue::glue("Cluster ID: ",{{cluster_name}}, " is not found in the dataset."))}
+  if(any(is.na(dataset[[cluster_name]]))) {stop(glue::glue("There are NAs in",{{cluster_name}},"."))}
+
+
+  df <- create_analysis_key_table(.results = result,analysis_key = analysis_key) |>
+    dplyr::select(dplyr::starts_with("group_var_")) |>
+    dplyr::select(-dplyr::starts_with("group_var_value_"))
+
+  all_group <- df |>  tidyr::unite(col = grouping_variable,everything(),na.rm =T,sep = " ~ ") |>
+    dplyr::pull(grouping_variable) |> unique()
+
+  grouping_variables <- stringr::str_split(all_group," ~ ") |>unlist() |> unique()
+
+  if(any(!grouping_variables %in% names(dataset))){
+    mgs <- grouping_variables[!grouping_variables %in% names(dataset)] |>
+      glue::glue_collapse(sep = ", ") %>%
+      glue::glue("Following grouping variables: ", ., " cannot be found in the dataset.")
+    stop(mgs)
+  }
+
+  final_df <- list()
+  for(i in all_group) {
+
+    grouping_variable <- stringr::str_split(i, " ~ ") |> unlist()
+
+    final_df[[i]] <- dataset |> dplyr::group_by(!!!rlang::syms(grouping_variable)) |> dplyr::summarise(
+      number_of_cluster = dplyr::n_distinct(!!rlang::sym(cluster_name)),
+      number_of_hh = dplyr::n()
+    ) |> tidyr::unite(col ="group_var_value", dplyr::all_of(grouping_variable),sep = " ~ ")
+
+
+  }
+
+  my_bind_row <- get("bind_rows",asNamespace("dplyr"))
+
+  do.call("my_bind_row",final_df) |> dplyr::ungroup()
+
+}
+
+
+
---
+++ tests/testthat/test-create_cluster_and_strata_summary.R
@@ -0,0 +1,67 @@
+testthat::test_that("check messeges", {
+
+  testthat::expect_error(create_group_clusters(result = presentresults::presentresults_resultstable,
+                                               analysis_key = "Analsysi_key",
+                                               dataset = presentresults::presentresults_MSNA_template_data,
+                                               cluster_name = "cluster_id"),"Analysis key: Analsysi_key is not found in the result table.")
+
+  testthat::expect_error(create_group_clusters(result = presentresults::presentresults_resultstable,
+                                               analysis_key = "analysis_key",
+                                               dataset = presentresults::presentresults_MSNA_template_data,
+                                               cluster_name = "clusterid"),"Cluster ID: clusterid is not found in the dataset.")
+
+
+  df_with_NA <- presentresults::presentresults_MSNA_template_data
+  df_with_NA$cluster_id[1] <- NA
+  testthat::expect_error(create_group_clusters(result = presentresults::presentresults_resultstable,
+                                               analysis_key = "analysis_key",
+                                               dataset = df_with_NA,
+                                               cluster_name = "cluster_id"),"There are NAs incluster_id.")
+
+  ## Grouping variable
+  result_table <- presentresults::presentresults_resultstable
+  result_table$analysis_key <-   result_table$analysis_key |> stringr::str_replace_all("population","pop")
+
+  testthat::expect_error(create_group_clusters(result = result_table,
+                                               analysis_key = "analysis_key",
+                                               dataset =  presentresults::presentresults_MSNA_template_data,
+                                               cluster_name = "cluster_id"),"Following grouping variables: pop cannot be found in the dataset.")
+
+})
+
+testthat::test_that("expect equal", {
+
+  actual <- create_group_clusters(result =  presentresults::presentresults_resultstable,
+                                  dataset =  presentresults::presentresults_MSNA_template_data,
+                                  cluster_name = "cluster_id")
+
+  expected <- structure(list(group_var_value = c("locationA ~ displaced", "locationA ~ non-displaced",
+                                                 "locationB ~ displaced", "locationB ~ non-displaced", "locationA",
+                                                 "locationB"), number_of_cluster = c(2L, 2L, 2L, 2L, 2L, 2L),
+                             number_of_hh = c(31L, 24L, 27L, 18L, 55L, 45L)), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -6L))
+
+  testthat::expect_equal(actual,expected)
+
+  df <-  presentresults::presentresults_MSNA_template_data
+
+  df$cluster_id <-sample(c("a","b","c"),size = 100,replace = T)
+
+  df$cluster_id <- dplyr::case_when( df$location == "locationA" ~ "x",T~ df$cluster_id)
+
+  actual <- create_group_clusters(result =  presentresults::presentresults_resultstable,
+                                  dataset = df,
+                                  cluster_name = "cluster_id")
+  expected <- structure(list(group_var_value = c("locationA ~ displaced", "locationA ~ non-displaced",
+                                                 "locationB ~ displaced", "locationB ~ non-displaced", "locationA",  "locationB"),
+                             number_of_cluster = c(1L, 1L, 3L, 3L, 1L, 3L),
+                             number_of_hh = c(31L, 24L, 27L, 18L, 55L, 45L)), class = c("tbl_df",  "tbl", "data.frame"),
+                        row.names = c(NA, -6L))
+
+  testthat::expect_equal(actual,expected)
+
+
+
+})
+
+
+
