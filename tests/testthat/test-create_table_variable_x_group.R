7d079d8e34e6bee42b1db66aed328f74383ebc82
Md Mehedi Hasan KHAN <plannermehedi@gamil.com>
2023-07-24 09:55:00 +0300

    fixing tests for create_table_variable_x_group

--- tests/testthat/test-create_table_variable_x_group.R
+++ tests/testthat/test-create_table_variable_x_group.R
@@ -1,57 +1,57 @@
-# test_that("returns the correct results", {
-#   results <- readRDS(testthat::test_path("fixtures", "results.rds"))
-#   expected_wide_table <-
-#     readRDS(testthat::test_path("fixtures", "widetable1.rds"))
-#
-#   expect_equal(
-#     create_table_variable_x_group(results, "key_index", "stat"),
-#     expected_wide_table
-#   ) %>%
-#     suppressWarnings()
-# })
-#
-# test_that("returns correct error message if index column is not correct.", {
-#   results <- readRDS(testthat::test_path("fixtures", "results.rds"))
-#   results <- results %>%
-#     dplyr::mutate(
-#       key_index = stringr::str_replace(key_index,
-#                                        pattern = " *[:alnum:] @/@",
-#                                        replacement = "XXXXXXXXXXXXX")
-#     )
-#   expected_wide_table <-
-#     readRDS(testthat::test_path("fixtures", "widetable1.rds"))
-#   expect_error(
-#     create_table_variable_x_group(results, "key_index", "stat"),
-#     "Analysis keys does not seem to follow the correct format"
-#   )
-# })
-#
-# test_that("returns the correct results with 3 stats", {
-#   results <- readRDS(testthat::test_path("fixtures", "results.rds"))
-#   expected_wide_table <-
-#     readRDS(testthat::test_path("fixtures", "widetable1_3stats.rds"))
-#
-#   expect_equal(create_table_variable_x_group(results,
-#                                              "key_index",
-#                                              c("stat", "stat_low", "stat_upp")),
-#                expected_wide_table) %>%
-#     suppressWarnings()
-# })
-#
-# test_that("returns a list per grouping variables", {
-#   results <- readRDS(testthat::test_path("fixtures", "results.rds"))
-#   expected_wide_table <-
-#     readRDS(testthat::test_path("fixtures", "widetable1_list_excel.rds"))
-#   expect_equal(
-#     create_table_variable_x_group(results,
-#                                   "key_index",
-#                                   c("stat"),
-#                                   list_for_excel = TRUE),
-#     expected_wide_table
-#   )  %>%
-#     suppressWarnings()
-#   ###the results of create_table_variable_x_group(results, "key_index",
-#   ###c("stat"), list_for_excel = TRUE) differs within the expect_equal and when
-#   ### running it. try with debugonce to see that
-#
-# })
+test_that("returns the correct results", {
+  results <- readRDS(testthat::test_path("fixtures", "results.rds"))
+  expected_wide_table <-
+    readRDS(testthat::test_path("fixtures", "widetable1.rds"))
+
+  expect_equal(
+    create_table_variable_x_group(results, "key_index", "stat"),
+    expected_wide_table
+  ) %>%
+    suppressWarnings()
+})
+
+test_that("returns correct error message if index column is not correct.", {
+  results <- readRDS(testthat::test_path("fixtures", "results.rds"))
+  results <- results %>%
+    dplyr::mutate(
+      key_index = stringr::str_replace(key_index,
+                                       pattern = " *[:alnum:] @/@",
+                                       replacement = "XXXXXXXXXXXXX")
+    )
+  expected_wide_table <-
+    readRDS(testthat::test_path("fixtures", "widetable1.rds"))
+  expect_error(
+    create_table_variable_x_group(results, "key_index", "stat"),
+    "Analysis keys does not seem to follow the correct format"
+  )
+})
+
+test_that("returns the correct results with 3 stats", {
+  results <- readRDS(testthat::test_path("fixtures", "results.rds"))
+  expected_wide_table <-
+    readRDS(testthat::test_path("fixtures", "widetable1_3stats.rds"))
+
+  expect_equal(create_table_variable_x_group(results,
+                                             "key_index",
+                                             c("stat", "stat_low", "stat_upp")),
+               expected_wide_table) %>%
+    suppressWarnings()
+})
+
+test_that("returns a list per grouping variables", {
+  results <- readRDS(testthat::test_path("fixtures", "results.rds"))
+  expected_wide_table <-
+    readRDS(testthat::test_path("fixtures", "widetable1_list_excel.rds"))
+  expect_equal(
+    create_table_variable_x_group(results,
+                                  "key_index",
+                                  c("stat"),
+                                  list_for_excel = TRUE),
+    expected_wide_table
+  )  %>%
+    suppressWarnings()
+  ###the results of create_table_variable_x_group(results, "key_index",
+  ###c("stat"), list_for_excel = TRUE) differs within the expect_equal and when
+  ### running it. try with debugonce to see that
+
+})
